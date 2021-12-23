676c6e52c851802296a666abc48b6128b4cdd472
krompac <tomijehrvat@gmail.com>
2021-12-23 11:17:12 +0100

    Initial board display.

--- ChessCpp/main.cpp
+++ ChessCpp/main.cpp
@@ -4,9 +4,26 @@
 int main()
 {
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 800;
+    const int numberOfSquares = 8;
 
-    InitWindow(screenWidth, screenHeight, "Window");
+    int squareSide = 85;
+
+    int initialXPos = (screenWidth - (squareSide * 8)) / 2;
+    int initialYPos = (screenHeight - (squareSide * 8)) / 2;
+    int x = initialXPos;
+    int y = initialYPos;
+
+    Color lightColor = { 226, 184, 126, 255 };
+    Color darkColor = { 143, 87, 54, 255 };
+
+    Color letterColor = { 238, 229, 217, 255 };
+
+    Color background = { 163,78,37,255 };
+
+    Rectangle rec = { x - 6.f, y - 6.f, squareSide * 8 + 12.f, squareSide * 8 + 12.f };
+
+    InitWindow(screenWidth, screenHeight, "Chess");
 
     SetTargetFPS(60);               
 
@@ -14,9 +31,33 @@ int main()
     {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(background);
 
-        DrawText("Hello there", 190, 200, 20, LIGHTGRAY);
+        DrawRectangleLinesEx(rec, 6, WHITE);
+
+        for (int i = 1; i <= numberOfSquares * 8; i++)
+        {
+            if (i % 2 == 0)
+            {
+                DrawRectangle(x, y, squareSide, squareSide, lightColor);
+            }
+            else
+            {
+                DrawRectangle(x, y, squareSide, squareSide, darkColor);
+            }
+            
+            if (i % 8 == 0) 
+            {
+                x = initialXPos;
+                y += squareSide;
+
+                std::swap(lightColor, darkColor);
+            }
+            else
+            {
+                x += squareSide;
+            }
+        }
 
         EndDrawing();
     }
